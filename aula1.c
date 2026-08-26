fc5f892cff057281f5d15dc6180877ee171c2c6f
Júlia Meneses <juhmeneses21@gmail.com>
2026-08-25 23:21:22 -0300

    primeira alteração

---
+++ aula1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+  float nota1, nota2, nota3, media;
+
+  nota1 = 10.0;
+  nota2 = 5.3;
+  nota3 = 8.9;
+  media = (nota1 + nota2 + nota3)/3;
+
+  printf("a media é: %.2f/n", media);
+
+  return 0;
+
+
+}
